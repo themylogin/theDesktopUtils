79d5bea7754cfb6f6771f49126ff89e33f51ba47
themylogin <themylogin@gmail.com>
2012-08-10 18:43:01 +0700

    LifeMetrics/MouseOdometer

---
+++ LifeMetrics/MouseOdometer.cpp
@@ -0,0 +1,40 @@
+// g++ -o MouseOdometer -lm -lX11 MouseOdometer.cpp
+#include <X11/Xlib.h>
+#include <X11/X.h>
+#include <math.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+ 
+int main(int argc, char* argv[])
+{
+    Display* display = XOpenDisplay(NULL);
+    Window root = RootWindow(display, DefaultScreen(display));
+
+    unsigned long long pixels = 0; // 2500px = 40mm
+    int root_x_last = 0, root_y_last = 0;
+    while (true)
+    {
+        pixels = 0;
+        for (int t = 0; t < 5 /* minutes */ * 60 /* seconds */ * 1000000 /* microseconds */ / 10000 /* poll interval */; t++)
+        {
+        	int root_x_return, root_y_return, win_x_return, win_y_return;
+    		Window root_return, child_return;
+    	    unsigned int mask_return;
+        	XQueryPointer(display, root, &root_return, &child_return, &root_x_return, &root_y_return, &win_x_return, &win_y_return, &mask_return);
+
+        	pixels += sqrt((root_x_last - root_x_return) * (root_x_last - root_x_return) + (root_y_last - root_y_return) * (root_y_last - root_y_return));
+        	root_x_last = root_x_return;
+        	root_y_last = root_y_return;
+
+        	usleep(10000);
+        }
+
+        FILE* fh = fopen(argv[1], "a");
+        fprintf(fh, "%d %d\n", time(NULL), pixels);
+        fclose(fh);
+    }
+
+    XCloseDisplay(display);
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+* LifeMetrics: Measure your life to achieve great success!
 * MusicJokes: MPD client, which writes "Music will finish at 22:50", "Music will never finish" or "Silence is boring, turn on the music!" to /tmp/musicjokes.txt depending on current playlist contents. You can show this information on your desktop with conky: `${tail /tmp/musicjokes.txt 1 1}`
 * MountManager: http://thelogin.ru/blog/post/safely-remove-hardware/
 * thexkb: http://thelogin.ru/blog/post/25dimetoksi4yodofenetilamin/
